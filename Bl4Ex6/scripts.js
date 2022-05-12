66e7750a4f6b766ae773035bd8344fa832480c71
PlinioMoraes <psmoraes@inf.ufpel.edu.br>
2022-05-12 18:12:51 -0500

    Mexendo com switch/case

---
+++ Bl4Ex6/scripts.js
@@ -0,0 +1,15 @@
+let situacaoPessoa = "aprovada";
+switch(situacaoPessoa){
+    case "aprovada":
+        console.log("aprovada");
+    break;
+    case "lista":
+        console.log("lista");
+    break;
+    case "reprovada":
+        console.log("reprovada");
+    break;
+
+    default: 
+        console.log("nao se aplica");
+}
