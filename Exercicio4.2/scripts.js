1d49f8fcfe66021a06673112b218153cad3916e6
PlinioMoraes <psmoraes@inf.ufpel.edu.br>
2022-05-16 10:20:24 -0500

    Exercicio 4.2.2

--- Exercicio4.2/scripts.js
+++ Exercicio4.2/scripts.js
@@ -1,8 +1,17 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-function runArray(numbers){ // printa todos itens do array
+function runArray(numbers){ // printa todos itens do array - Exercicio 1
     for(let index =0; index<numbers.length; index += 1 ){ // percorre o array
         console.log(numbers[index]); //printa a posicao index do array numbers
     }
 }
 //runArray(numbers); // chama a funcao de printa array
+
+function somaArray(numbers){ // funcao soma itens do array - Exercicio 2
+    let resultadoSoma = 0; // variavel que armazena a soma de todos itens do array
+    for(let index =0; index<numbers.length; index += 1 ){ // percorre o array
+        resultadoSoma += numbers[index]; //soma mais uma posicao do array
+    }
+    console.log(resultadoSoma); // printa o resultado da soma de todos itens do array
+}
+//somaArray(numbers); // chama a funcao
