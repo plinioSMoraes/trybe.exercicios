bd63d9ee347433bab222b572db5292ef72b42982
PlinioMoraes <psmoraes@inf.ufpel.edu.br>
2022-05-16 14:42:08 -0500

    Exercicio 4.3.2

--- Aula4.3/Exercicio4.3/scripts.js
+++ Aula4.3/Exercicio4.3/scripts.js
@@ -5,4 +5,13 @@ function fatorial(number){
     }
     console.log(resultado); // printa o resultado
 }
-fatorial(4); // chama a funcao
+// fatorial(4); // chama a funcao
+
+function inverteWord(palavra){ // inverte uma palavra
+    let newWord = ""; // cria uma palavra nova, porque nao da pra modificar strings
+    for(let index = palavra.length-1; index >= 0; index -= 1){
+        newWord = newWord + palavra[index]; // concatena a ultima letra da string com a palavra criada
+    }
+    console.log(newWord); // printa a palavra invertida
+}
+// inverteWord("tryber"); // chama a funcao inverte palavras
