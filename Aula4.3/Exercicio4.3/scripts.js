af3f5f3050a8751fbf17eadcb91ffb38f93894ca
PlinioMoraes <psmoraes@inf.ufpel.edu.br>
2022-05-16 15:41:07 -0500

    Exercicio 4.3.4

--- Aula4.3/Exercicio4.3/scripts.js
+++ Aula4.3/Exercicio4.3/scripts.js
@@ -33,3 +33,32 @@ function maiorEMenorPalavra(arrayWords){ // printa a maior e a menor palavra de 
     console.log("A menor palavra é: " + smallestWord);
 }
 //maiorEMenorPalavra(array);
+
+
+// Exercicio 4.3.4
+function ePrimo(number){ // diz se o numero é primo
+    let divisors = 0; // quantidade de divisores de um numero
+    for(let index = 1; index <=number; index += 1){ // percorre o numero dividindo ele pelos seus antecessores
+        if(number%index == 0){
+            divisors += 1;
+        }
+    }
+    if(divisors <=2){ // se divisores maior que 2, nao e primo
+        return "true";
+    }else{
+        return "false";
+    } 
+}
+function maiorPrimo(number){ // retorna o maior numero primo dos antecessores de um numero
+    let maiorPrimo = 0; // seta o maior primo como 0
+    for(let index = 1; index <= number; index += 1){ //percorre os antecessores do numero pra ver qual e primo
+        let primo = ePrimo(index); // chama a funcao pra ver se o numero e primo
+        if(primo == "true"){ // se for primo, ve se ele e maior que o primo setado
+            if(maiorPrimo < index){ // se for seta ele como novo primo
+                maiorPrimo = index;
+            }
+        }
+    }
+    console.log(maiorPrimo); // retorna o maior primo entre os antecessores do numero ou ele mesmo
+}
+maiorPrimo(10000); // chama a funcao
