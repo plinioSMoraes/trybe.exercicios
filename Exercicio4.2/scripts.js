4c0ecaa7c830b62bb6ef921600fc506b07be9d4b
PlinioMoraes <psmoraes@inf.ufpel.edu.br>
2022-05-16 10:06:10 -0500

    Exercicio 4.2.1

---
+++ Exercicio4.2/scripts.js
@@ -0,0 +1,8 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+function runArray(numbers){ // printa todos itens do array
+    for(let index =0; index<numbers.length; index += 1 ){ // percorre o array
+        console.log(numbers[index]); //printa a posicao index do array numbers
+    }
+}
+//runArray(numbers); // chama a funcao de printa array
