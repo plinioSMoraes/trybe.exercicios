95a36f26df5908c15452865e95e338237fb79e41
PlinioMoraes <psmoraes@inf.ufpel.edu.br>
2022-05-17 15:32:12 -0500

    Exemplo 4.4.2

---
+++ Aula4.4/Example1.2/scripts.js
@@ -0,0 +1,8 @@
+// Exemplo 1.2 Bloco 4.4
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim'
+  };
+console.log(info.recorrente);
